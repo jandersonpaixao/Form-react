7858b85bc57e77fa548aba2a6a9641c02477eb6a
jandersonpaixao <janderson.paixao@sanar.com>
2024-07-31 22:24:07 -0300

    feat: add infoBox component at CourseGoalList

--- src/components/CourseGoalList/CourseGoalList.tsx
+++ src/components/CourseGoalList/CourseGoalList.tsx
@@ -1,5 +1,8 @@
+import { type ReactNode } from "react";
+
 import { useGoalProps as Goals } from "../../App";
 import CourseGoal from "../CurseGoal/CourseGoal";
+import { InfoBox } from "../InfoBox/InfoBox";
 
 type CourseGoaListProps = {
   goals: Goals[];
@@ -7,16 +10,35 @@ type CourseGoaListProps = {
 };
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoaListProps) => {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        <p>you don't have goals yet. Try to add some goals!</p>
+      </InfoBox>
+    );
+  }
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="medium">
+        <p>you're adding too many goals. take it easy...</p>
+      </InfoBox>
+    );
+  }
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
